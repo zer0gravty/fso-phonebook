583066a78945c16eb063b26d8ed800269754d943
Frank Vamos, Jr <fvamos@gmail.com>
2021-09-29 22:24:46 -0400

    Remove info; set up env file

---
+++ configs.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+const configs {
+    MONGO_DB_NAME,
+    MONGO_URL,
+    MONGO_USER,
+    MONGO_PASSWORD,
+};
+
+export default configs;
