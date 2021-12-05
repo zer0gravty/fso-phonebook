fe7d2f62a027bf5d6925825a515d303f42f7cf35
Frank Vamos, Jr <fvamos@gmail.com>
2021-12-04 21:07:51 -0500

    Finished project 3.3

--- index.js
+++ index.js
@@ -10,21 +10,35 @@ app.use(express.json());
 morgan.token('post-body', (req) => Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : '');
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'));
 
+// error handler function; last middleware to load towards end of this file
+const errorHandler = (err, _req, res, next) => {
+  console.log(`Error: ${err}`);
+
+  if (err.name === 'CastError') {
+    res.status(400).json({ error: 'malformed id' });
+  }
+
+  next(err);
+};
+
 // serve static files from React build
 app.use(express.static('build'));
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(people => res.json(people));
+app.get('/api/persons', (_req, res, next) => {
+  Person.find({}).then(people => res.json(people)).catch(err => next(err));
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => person ? res.json(person) : res.status(404).end())
+    .catch(err => next(err));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  Person.findById(id).then(person => res.json(person));
-});
-
-app.post('/api/persons', (req, res) => {
-  if (!req.body.name || !req.body.number) {
-    return res.status(400).send({ error: 'Malformed body in request.' });
+app.post('/api/persons', (req, res, next) => {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Malformed body in request: missing name.' });
+  } else if (!req.body.number) {
+    return res.status(400).json({ error: 'Malformed body in request: missing number.' });
   }
 
   const newPerson = new Person({
@@ -32,29 +46,41 @@ app.post('/api/persons', (req, res) => {
     number: req.body.number,
   });
 
-  newPerson.save().then(payload => res.status(201).json(payload));
+  newPerson.save().then(payload => res.status(201).json(payload)).catch(err => next(err));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const person = data.find((person) => person.id === id);
-  if (person) {
-    data = data.filter((person) => person.id !== id);
-    res.status(204).end();
-  } else {
-    res
-      .status(400)
-      .json({ message: `Unable to delete person with id of ${id}` });
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(_result => res.status(204).end())
+    .catch(err => next(err));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  }
+  
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => res.status(201).json(updatedPerson))
+    .catch(err => next(err));
+});
+
+app.get('/info', async (_req, res, next) => {
+  const date = new Date();
+  try {
+    const payload = await Person.find({});
+    const numOfPeople = await payload?.length || 0;
+    res.send(`
+      <h1>Phonebook has info for ${numOfPeople} people<h1>
+      <p>${date.toString()}</p>
+    `);
+  } catch (err) {
+    next(err);
   }
 });
 
-app.get('/info', (req, res) => {
-  const date = new Date();
-  res.send(`
-    <h1>Phonebook as info for ${data.length} people<h1>
-    <p>${date.toString()}</p>
-  `);
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
